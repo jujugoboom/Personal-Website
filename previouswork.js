6f6dc97b84a8c2133ce19eb56f59e17422d31d28
jujugoboom <jujugoboom@gmail.com>
2016-03-28 10:42:40 -0400

    Added styling and cleaned up

--- previouswork.js
+++ previouswork.js
@@ -1,6 +1,6 @@
 var input = document.getElementById("input-block");
 var content = document.createElement("p");
 content.style.color = "green";
-content.innerHTML = "My previous work can be found <a href='http://github.com/jujugoboom'>here</a>";
+content.innerHTML = "My previous work can be found <button onclick=\'location.href=\"http://github.com/jujugoboom\"\'>here</button>";
 input.appendChild(content);
 reset();
