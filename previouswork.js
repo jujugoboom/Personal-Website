4d9db613dde2130832cddf1935f608e3af0e4952
jujugoboom <jujugoboom@gmail.com>
2016-03-29 12:32:46 -0400

    Open link in new tab, instead of in current tab

--- previouswork.js
+++ previouswork.js
@@ -1,6 +1,6 @@
 var input = document.getElementById("input-block");
 var content = document.createElement("p");
 content.style.color = "green";
-content.innerHTML = "My previous work can be found <button onclick=\'location.href=\"http://github.com/jujugoboom\"\'>here</button>";
+content.innerHTML = "My previous work can be found <button onclick=\'window.open(\"http://github.com/jujugoboom\", \"_blank\")\'>here</button>";
 input.appendChild(content);
 reset();
