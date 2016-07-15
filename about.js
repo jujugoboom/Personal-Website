72671cea204b093020a74ec8fe9ee67f03061586
jujugoboom <jujugoboom@gmail.com>
2016-07-14 20:51:50 -0400

    Edit about me

--- about.js
+++ about.js
@@ -1,5 +1,5 @@
 var input = document.getElementById("input-block");
 var content = document.createElement("p");
-content.innerHTML = "I am a freshman in college at RIT. I am majoring in Computer Science, and I love it.";
+content.innerHTML = "I am a college student studying computer science. I currently live in Maryland, and go to school at the University of Maryland";
 input.appendChild(content);
 reset();
